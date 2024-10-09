812d9c009107299e52f2090173962a0d4fb5819e
Zahran24 <zahranhanif1@gmail.com>
2024-10-09 13:55:42 +0700

    second

--- reclean.py
+++ reclean.py
@@ -2,22 +2,66 @@ import pandas as pd
 import re
 import os
 from textblob import TextBlob
+import html  # Untuk menghapus entitas HTML lebih efisien
 
 # Path file input
 excel_file_path = 'processed_data.xlsx'
 
+# Daftar kata-kata informal yang ingin dilewati
+informal_words = [
+    'ain’t', 'ASAP', 'babe', 'bday', 'bff', 'bffls', 'bet', 'bby', 'brb', 'bruh', 'chillin', 
+    "c'mon", 'cu', 'cuz', 'dead', 'dawg', 'dunno', 'damn', 'dam', 'emo', 'fam', 'friggin', 
+    'fr', 'frfr', 'gr8', 'graceeeeee', 'gimme', "g'morning", 'gonna', 'gotta', 'gunna', 
+    'h', 'hmu', 'hell no', 'hell yes', 'hella', "how's it goin", 'id', 'idk', 'ikr', 'im', 
+    'it’s all good', 'jk', 'k', 'kinda', 'l', 'lame', 'lemme', 'lol', 'lmao', 'm8', 'me thinks', 
+    'n', 'n/a', 'probs', 'p', 'pissed', 'plz', 'rn', 'rip', 's', 'smh', 'sm', 'sry', 'sup', 
+    'tbh', 'tis', 'tmi', 'tired of everything', 'ty', 'u', 'ur', 'w', 'w/', 'wtf', 'who tf', 
+    'w/', 'xoxo', 'ya', 'y', 'yep', 'yolo'
+]
+
+# Fungsi untuk mengoreksi kata jika tidak ada dalam list informal
+def correct_word(word):
+    # Jika kata ada dalam daftar kata informal, biarkan tetap
+    if word.lower() in informal_words:
+        return word
+    
+    # Normalisasi huruf berulang (misalnya "goooood" menjadi "good")
+    word = re.sub(r'(.)\1{2,}', r'\1', word)
+    
+    # Koreksi ejaan menggunakan TextBlob
+    blob = TextBlob(word)
+    try:
+        return str(blob.correct())
+    except Exception as e:
+        print(f"Error pada TextBlob: {e}")
+        return word  # Kembali ke kata asli jika terjadi error
+
+# Fungsi untuk mengoreksi seluruh kalimat
+def correct_sentence(sentence):
+    if not isinstance(sentence, str):
+        return ''  # Jika bukan string (misalnya NaN), kembalikan string kosong
+    
+    # Pisahkan kalimat menjadi kata-kata
+    words = sentence.split()
+    
+    # Koreksi tiap kata satu per satu
+    corrected_words = [correct_word(word) for word in words]
+    
+    # Gabungkan kata-kata yang sudah dikoreksi menjadi kalimat
+    return ' '.join(corrected_words)
+
 try:
     # Membaca file Excel
-    df = pd.read_excel(excel_file_path)  # read file excel
-    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]  # hapus kolom kosong
+    df = pd.read_excel(excel_file_path)  # Membaca file excel
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]  # Menghapus kolom kosong
 
     # Menghapus username Twitter, karakter non-ASCII, URL, hashtag, dan entitas HTML
-    df['Tweet'] = df['Tweet'].str.replace(r'@\w+', '', regex=True)  # hapus username
-    df['Tweet'] = df['Tweet'].str.replace(r'[^\x00-\x7F]+', '', regex=True)  # hapus karakter non-ASCII
-    df['Tweet'] = df['Tweet'].str.replace(r'https?://\S+', '', regex=True)  # hapus URL
-    df['Tweet'] = df['Tweet'].str.replace(r'#\w+', '', regex=True)  # hapus hashtag
-    df['Tweet'] = df['Tweet'].str.replace('&quot', '', regex=False)  # hapus entitas HTML &quot
-    df['Tweet'] = df['Tweet'].str.replace('&lt', '', regex=False)  # hapus entitas HTML &quot
+    df['Tweet'] = df['Tweet'].astype(str)  # Konversi semua entri menjadi string
+    df['Tweet'] = df['Tweet'].str.replace(r'@\w+', '', regex=True)  # Hapus username
+    df['Tweet'] = df['Tweet'].str.replace(r'[^\x00-\x7F]+', '', regex=True)  # Hapus karakter non-ASCII
+    df['Tweet'] = df['Tweet'].str.replace(r'https?://\S+', '', regex=True)  # Hapus URL
+    df['Tweet'] = df['Tweet'].str.replace(r'#\w+', '', regex=True)  # Hapus hashtag
+    df['Tweet'] = df['Tweet'].apply(lambda x: html.unescape(x))  # Menghapus entitas HTML (lebih menyeluruh)
 
     # Menghapus emotikon seperti XDDDD, :-D, :-P
     emoticons_pattern = r'X+D+|:-[DPdp]'  # Regex untuk XDDDD dan variasi :-D, :-P
@@ -26,19 +70,8 @@ try:
     # Normalisasi kata dengan huruf berulang (contoh: Gooooood -> good)
     df['Tweet'] = df['Tweet'].str.replace(r'(.)\1{2,}', r'\1', regex=True)
 
-    # Mengatasi NaN dengan mengisi entri yang kosong
-    df['Tweet'] = df['Tweet'].fillna('')  # Mengganti NaN dengan string kosong
-
-    # Koreksi ejaan otomatis menggunakan TextBlob (hanya untuk teks yang tidak kosong)
-    def correct_text(text):
-        if text.strip():  # Hanya proses jika teks tidak kosong
-            blob = TextBlob(text)
-            return str(blob.correct())
-        return text  # Kembalikan teks aslinya jika kosong
-
-    df['Tweet'] = df['Tweet'].apply(correct_text)
-
-    # print(df.head(10)) #print hasil
+    # Terapkan koreksi teks pada setiap tweet
+    df['Tweet'] = df['Tweet'].apply(correct_sentence)
 
     # Path untuk menyimpan file
     output_directory = 'C:/Users/Zahran/Documents/Punya Zahran/Zahran Unpad/Skripsi New/Code/Skripsi'
